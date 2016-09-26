83234b58eabf00126c46895a141bf4031f21389c
sahangitoffice <sahan@hsenidlanka.com>
2016-09-26 11:15:18 +0530

    Initial Commit

---
+++ src/main/java/com/executor/Executor1.java
@@ -0,0 +1,19 @@
+package com.executor;
+
+import com.allservices.*;
+
+
+/**
+ * Main method to execute all methods
+ * Created by hsenid on 9/21/16.
+ */
+public class Executor1 {
+    public static void main(String[] args) {
+        AllServices.ex1();
+        AllServices.fibonacciMethod();
+        AllServices.infinite();
+        AllServices.factorialMethod();
+        AllServices.multiplicationMethod();
+        AllServices.multiDimenPrint();
+    }
+}
